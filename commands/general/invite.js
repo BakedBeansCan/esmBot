19328fe1a796dfa4ceb229e56e064f6002a509b1
BakedBeansCan <dragonboy@velocity.net>
2022-01-27 20:48:36 -0500

    update to invite
    how did i miss this?

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -2,7 +2,7 @@ import Command from "../../classes/command.js";
 
 class InviteCommand extends Command {
   async run() {
-    return "You can invite me to your server here: <https://projectlounge.pw/invite>";
+    return "You can invite me to your server here: <https://discord.com/api/oauth2/authorize?client_id=908869206320222230&permissions=8&scope=bot>";
   }
 
   static description = "Gets my invite link";
